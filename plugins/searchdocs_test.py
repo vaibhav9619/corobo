1c0ff88334d5ff47725a2d7e4f8e6fc91ad10e8a
Vaibhav Sadana <sadana.vaibhav@gmail.com>
2018-08-02 02:11:15 +0530

    as

--- plugins/searchdocs_test.py
+++ plugins/searchdocs_test.py
@@ -6,9 +6,9 @@ extra_plugin_dir = 'plugins'
 def test_search_cmd(testbot):
     testbot.assertCommand(
         '!search api this is search string',
-        'http://api.coala.io/en/latest/search.html?q=this+is+search+string')
+        'https://api.coala.io/en/latest/search.html?q=this+is+search+string')
     testbot.assertCommand(
         '!search user this is search string',
-        'http://docs.coala.io/en/latest/search.html?q=this+is+search+string')
+        'https://docs.coala.io/en/latest/search.html?q=this+is+search+string')
     testbot.assertCommand('!search not matching',
                           'Invalid syntax')
